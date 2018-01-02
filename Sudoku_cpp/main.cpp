29c46c9c339c469c8822dedd81f36d16dc724dd3
Morgan Borjigin-Wang <mbwang10@gmail.com>
2018-01-02 17:50:59 -0500

    Initial Commit

---
+++ Sudoku_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku_cpp
+//
+//  Created by Morgan Borjigin-Wang on 1/2/18.
+//  Copyright © 2018 Morgan Borjigin-Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
